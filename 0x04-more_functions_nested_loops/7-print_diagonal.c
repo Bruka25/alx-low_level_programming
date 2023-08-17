189915fb599861793d6fef362253dad40cca0a26
Bruka25 <bkteshome862@gmail.com>
2023-08-17 09:52:00 -0700

    diago\

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+  * print_diagonal - Draws a diagonal line to the argument
+  * @n: The number of times to print diagonal lines
+  *
+  * Return: void
+  */
+void print_diagonal(int n)
+{
+	int a, b;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < n; x++)
+		{
+			for (b = 0; b < a; b++)
+			{
+				_putchar(32);
+			}
+
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
