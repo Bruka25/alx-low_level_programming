7d992fcf8395e671faa53a47b6359d22a6246ed4
Bruka25 <bkteshome862@gmail.com>
2023-08-17 08:49:00 -0700

    print

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * print_numbers - Print the numbers since 0 up to 9
+  *
+  * Return: The numbers since 0 up to 9
+  */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; x <= n; n++)
+	{
+		_putchar(n + '0');
+	}
+
+	_putchar('\n');
+}
