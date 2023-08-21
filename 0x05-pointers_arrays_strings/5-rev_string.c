7322944ab9446ade4c81e53fad04c85afa52f08f
Bruka25 <bkteshome862@gmail.com>
2023-08-21 11:35:14 -0700

    string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *rev_string - reverses string
+ *
+ *@s: The string that is reversed
+ *
+ *Return: void
+ */
+
+void rev_string(char *s)
+{
+	int c, d;
+	char r[1000];
+
+
+	c = d = 0;
+	while (s[c] != '\0')
+	{
+		r[c] = s[c];
+		c++;
+	}
+	c--;
+	while (i >= 0)
+	{
+		s[c] = r[d];
+		c--;
+		d++;
+	}
+	s[d++] = '\0';
+}
