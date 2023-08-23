614800d5c61a6ca6b1f759d00017d7188367a8a7
Bruka25 <bkteshome862@gmail.com>
2023-08-23 13:19:11 -0700

    comp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *_strcmp - compares two strings
+ *
+ *@s1: First string to be compared
+ *@s2: Second string to be compared
+ *
+ *Return: int of the string
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+	return (0);
+}
