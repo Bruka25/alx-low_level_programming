a377695b99b2a387562b70da77b96bb169d346c5
Bruka25 <bkteshome862@gmail.com>
2023-08-23 15:48:02 -0700

    lit2

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,21 +8,16 @@
 
 char *leet(char *str)
 {
-	int i;
+	int i, j;
+	char subs[] = "aAeEoOtTlL";
+	char lit[] = "43071";
 
+	i = 0;
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		while (str[i] == 'a' || str[i] == 'A')
-			str[i] = '4';
-		while (str[i] == 'e' || str[i] == 'E')
-			str[i] = '3';
-		while (str[i] == 'o' || str[i] == 'O')
-			str[i] = '0';
-		while (str[i] == 't' || str[i] == 'T')
-			str[i] = '7';
-		while (str[i] == 'l' || str[i] == 'L')
-			str[i] = '1';
+		for (j = 0; subs[j] != '\0'; j++)
+			if (str[i] == subs[j])
+				str[i] = lit[j / 2];
 	}
-
 	return (str);
 }
