3ac95b8da048295e140a4def6c8d6752d61eb331
Bruka25 <bkteshome862@gmail.com>
2023-08-24 10:53:30 -0700

    123

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,33 +1,32 @@
 #include "main.h"
 
 /**
- *rot13 - prints encoded string using rot13
+ * rot13 - encodes a string using ROT13 cipher
  *
- *@str: String to be encoded
+ * @str: String to be encoded
  *
- *Return: Encoded string
+ * Return: Encoded string
  */
 
 char *rot13(char *str)
 {
+	char storeh[] = "NOPQRSTUVWXYZABCDEFGHIJKLM";
+	char storel[] = "nopqrstuvwxyzabcdefghijklm";
 	int i = 0;
 
-	while (str[i])
+	while (str[i] != '\0')
 	{
-		while ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+		if ((str[i] >= 'a' && str[i] <= 'z') ||
+		    (str[i] >= 'A' && str[i] <= 'Z'))
 		{
-			if ((str[i] >= 'm' && str[i] <= 'z') || (str[i] >= 'M' && str[i] <= 'Z'))
-			{
+			int shift = (str[i] >= 'a' && str[i] <= 'z') ? 'a' : 'A';
+
+			if ((str[i] - shift) > 12)
 				str[i] -= 13;
-				break;
-			}
-
-			str[i] += 13;
-			break;
+			else
+				str[i] += 13;
 		}
-
 		i++;
 	}
-
 	return (str);
 }
