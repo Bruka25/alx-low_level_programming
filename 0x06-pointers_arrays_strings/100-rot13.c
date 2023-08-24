a27ea0320b19e35a8e5eab655d9fb615e1086508
Bruka25 <bkteshome862@gmail.com>
2023-08-24 10:32:29 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,13 +10,13 @@
 
 char *rot13(char *str)
 {
-	int i;
+	int i = 0;
 
 	while (str[i])
 	{
 		while ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
 		{
-			if ((str[i] > 'm' && str[i] < 'z') || (str[i] > 'M' && str[i] < 'Z'))
+			if ((str[i] >= 'm' && str[i] <= 'z') || (str[i] >= 'M' && str[i] <= 'Z'))
 			{
 				str[i] -= 13;
 				break;
