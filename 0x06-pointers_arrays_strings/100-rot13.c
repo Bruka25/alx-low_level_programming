2e08dd6f2b3660609a806751228a707b5d549d7c
Bruka25 <bkteshome862@gmail.com>
2023-08-24 09:54:58 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ *rot13 - prints encoded string using rot13
+ *
+ *@str: String to be encoded
+ *
+ *Return: Encoded string
+ */
+
+char *rot13(char *str)
+{
+	int i;
+
+	while (str[i])
+	{
+		while ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+		{
+			if ((str[i] > 'm' && str[i] <= 'z') || (str[i] > 'M' && str[i] <= 'Z'))
+			{
+				str[str] -= 13;
+				break;
+			}
+
+			s[str] += 13;
+			break;
+		}
+
+		i++;
+	}
+
+	return (str);
+}
