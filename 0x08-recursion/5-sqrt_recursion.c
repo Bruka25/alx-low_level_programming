d25656b4a20e5629b6c4cb86e38bbd000e879c57
Bruka25 <bkteshome862@gmail.com>
2023-08-30 09:29:20 -0700

    square root recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,19 +1,6 @@
 #include "main.h"
 
 /**
- *_sqrt_recursion - Function that prints a square root of a number
- *
- *@n: The number to be tested
- *
- *Return: Number after square root
- */
-
-int _sqrt_recursion(int n)
-{
-	return (guess(n, 1));
-}
-
-/**
  * guess - guesses the square root recursively
  * @h: number to be compared to sqrt
  * @i: integer to be incremented to find square root
@@ -31,4 +18,17 @@ int guess(int h, int i)
 		return (sqrt(h, i + 1));
 	else
 		return (-1);
+}
+
+/**
+ *_sqrt_recursion - Function that prints a square root of a number
+ *
+ *@n:Number to be printed
+ *
+ *Return: Number after square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (guess(n, 1));
 }
