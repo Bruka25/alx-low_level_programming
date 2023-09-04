ab45c0f1c81b1da5070a8ba0b7b3574c6bc9fa28
Bruka25 <bkteshome862@gmail.com>
2023-09-04 15:00:13 -0700

    2d grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,29 +12,30 @@
 
 int **alloc_grid(int width, int height)
 {
+	int i, j;
 	int **array;
-	int y, z;
 
-	if (width + height < 2 || width < 1 || height < 1)
+	i = j = 0;
+	if (height < 1)
 		return (NULL);
-
-	array = malloc(height * sizeof(*array));
+	array = (int **)malloc(height * sizeof(array));
 	if (array == NULL)
-		return (NULL);
-
-	for (y = 0; y < height; y++)
 	{
-		array[y] = malloc(width * sizeof(**array));
-		if (array[y] == NULL)
+		free(array);
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		array[i] = malloc(width * sizeof(int));
+		if (array[i] == NULL)
 		{
-			for (y--; y >= 0; y--)
-				free(array[y]);
+			for (j = 0; j < i; j++)
+				free(array[j]);
 			free(array);
 			return (NULL);
 		}
-		for (z = 0; z < width; z++)
-			array[z][y] = 0;
+		for (j = 0; j < width; j++)
+			array[i][j] = 0;
 	}
-
 	return (array);
 }
