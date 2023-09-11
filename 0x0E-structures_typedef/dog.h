808e4cd9e8cbba664122d2350879aed121289ec2
Bruka25 <bkteshome862@gmail.com>
2023-09-11 10:08:37 -0700

    dog header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG_HEADER
-#define DOG_HEADER
+#ifndef DOG_H
+#define DOG_H
 
 /**
  *struct dog - struct for the given dog attributes
@@ -16,14 +16,6 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-};
-
-
-
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+}
 
 #endif
