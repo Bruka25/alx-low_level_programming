9356360e3db1f379e52e8bc963eab887941dd45e
Bruka25 <bkteshome862@gmail.com>
2023-09-11 08:09:38 -0700

    dod header

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,11 @@
+The following c programs are for ALX 0x0E-structures_typedef project, inside this project  the following C functions are included:
+
+* A new type with the following elements:
+   name, type = char *
+   age, type = float
+   owner, type = char *
+* A function that initialize a variable of type struct dog
+* A function that prints a struct dog
+* A new type dog_t as a new name for the type struct dog
+* A function that creates a new dog
+* A  function that frees dogs 
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,29 @@
+#ifndef DOG_HEADER
+#define DOG_HEADER
+
+/**
+ *struct dog - struct for the given dog attributes
+ *
+ *@name: Name of the dog
+ *@age: age of the dog
+ *@owner: Owner of the dog
+ *
+ *Description: Lists the attributes of the dog.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
