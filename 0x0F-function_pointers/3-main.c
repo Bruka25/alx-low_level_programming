39044ebbc9bc60e281c736755cafa32f6774c78b
Bruka25 <bkteshome862@gmail.com>
2023-09-13 13:14:35 -0700

    simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,31 +11,31 @@
  * Return: 0 on success, print Error and exit with 98 if incorrect
  *         number of args, exit with 99 if the operator given is incorrect
  */
+
 int main(int argc, char *argv[])
 {
 	int (*operation)(int, int);
 	int num1, num2, res;
 
-	if (argc == 4)
+	if (argc != 4)
 	{
-		if (argv[2][1] != '\0')
-		{
-			printf("Error\n");
-			exit(99);
-		}
-		operation = get_op_func(argv[2]);
-		if (operation == NULL)
-		{
-			printf("Error\n");
-			exit(99);
-		}
+		printf("Error\n");
+		exit(98);
+	}
 
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[3]);
-		res = operation(num1, num2);
+	operation = get_op_func(argv[2]);
 
-		printf("operation response %d\n", res);
-		return (0);
+	if (!operation)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	res = operation(num1, num2);
+
+	printf("operation response %d\n", res);
+	return (0);
 	}
 	else
 	{
