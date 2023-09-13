782af924d41fd49c3c251d9aa865c58843d06895
Bruka25 <bkteshome862@gmail.com>
2023-09-13 14:36:47 -0700

    simple operation

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -55,8 +55,14 @@ int op_mul(int a, int b)
 
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a / b);
 }
+
 
 /**
  * op_mod - Calculates remainder of a and b
@@ -68,5 +74,10 @@ int op_div(int a, int b)
 
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
