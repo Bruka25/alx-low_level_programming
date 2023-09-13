5c7a6cfa40cc6ccb1ad3661b82e15392142aa28d
Bruka25 <bkteshome862@gmail.com>
2023-09-13 11:49:58 -0700

    simple operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * op_add - Adds two numbers
+ *
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: Sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Substracts two numbers
+ *
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: Difference of the numbers
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Multiplies two numbers
+ *
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: The product of a and b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Divides a and b
+ *
+ * @a: First number
+ * @b: The devidend
+ *
+ * Return: Quotient of the numbers
+ */
+
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - Calculates remainder of a and b
+ * @a: First number
+ * @b: The devidend
+ *
+ * Return: Remainder of a and b
+ */
+
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
