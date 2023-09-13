110599a10660fb4d770f5840b2e8f1756009b010
Bruka25 <bkteshome862@gmail.com>
2023-09-13 14:07:53 -0700

    operation simple

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,45 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Where the program Runs
+ *
+ * @argc: Number of argument count
+ * @argv: Pointer to the argument
+ *
+ * Return: 0 on success, print Error and exit with 98 if incorrect
+ *         number of args, exit with 99 if the operator given is incorrect
+ */
+
+{
+	int num1, num2;
+	char *operation;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	operation = argv[2];
+	num2 = atoi(argv[3]);
+
+	if (get_op_func(operation) == NULL || operation[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*operation == '/' && num2 == 0) ||
+	    (*operation == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("operation response %d\n", get_op_func(operation)(num1, num2));
+
+	return (0);
+}
