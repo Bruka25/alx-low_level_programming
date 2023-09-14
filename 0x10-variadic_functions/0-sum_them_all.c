4a177c181df0817ba693d503e3b3296f02e67b25
Bruka25 <bkteshome862@gmail.com>
2023-09-14 07:43:06 -0700

    sum them all

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ *sum_them_all - Sums the parameters
+ *
+ *@n: The first variadic argument
+ *
+ *Return: The result of the sum
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int a;
+	unsigned int sum;
+
+	a = sumarg = 0;
+
+	va_start(args, n);
+	if (n == 0)
+		return (0);
+	while (a < n)
+	{
+		sumarg += va_arg(args, int);
+		a++;
+	}
+
+	va_end(args);
+	return (sumarg);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,6 @@
+The following programs are for ALX 0x10-variadic_functions assignment, inside it contains the following functions:
+
+* A function that returns the sum of all its parameters
+* A function that prints numbers, followed by a new line
+* A function that prints strings, followed by a new line
+* A function that prints anything 
