a015ad66d50f9369a7c673dc134a47f551647b33
Bruka25 <bkteshome862@gmail.com>
2023-09-26 04:56:34 -0700

    printsafe fix with other prototypes

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,40 +1,89 @@
 #include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  *print_listint_safe - Function that prints a linked list in safe manner
  *
  *@head: Pointer to the head of the list
  *
- *Return: Number of nodes
+ *Return: Number of nodes or exit 98 if error
  */
 
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *current_node;
-	size_t node_num;
-	const listint_t *temp;
-
-	current_node = head;
-	if (current_node == NULL)
-		exit(98);
+	listadd_t *new_head;
+	listadd_t *check_loop;
+	unsigned int node_num;
 
 	node_num = 0;
+	new_head = NULL;
 
-	while (current_node != NULL)
+	while (head != NULL)
 	{
-		temp = current_node;
-		current_node = current_node->next;
-		node_num++;
-		printf("[%p] %d\n", (void *)temp, temp->n);
-
-		if (temp < current_node)
+		check_loop = new_head;
+		while (check_loop != NULL)
 		{
-			printf("-> [%p] %d\n", (void *)current_node, current_node->n);
-			break;
+			if (head == check_loop->ptr)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free_list(new_head);
+				return (node_num);
+			}
+			check_loop = check_loop->next;
 		}
+		printf("[%p] %d\n", (void *)head, head->n);
+		add_node(&new_head, head);
+		head = head->next;
+		node_num++;
 	}
-
+	free_list(new_head);
 	return (node_num);
+}
+
+
+/**
+ *add_node - Add a node to the linked list
+ *
+ *@head: Pointer to the pointer the head of the list
+ *@ptr: The address of another list's node
+ *
+ *Return: Address of new node, NULL if it fails
+ */
+
+listadd_t *add_node(listadd_t **head, const listint_t *ptr)
+{
+	listadd_t *new_node;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+	{
+		free_list(*head);
+		exit(98);
+	}
+	new_node->ptr = (void *)ptr;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
+
+/**
+ *free_list - Frees the linked list
+ *
+ *@head: Pointer to the head of the linked list
+ *
+ *Return: Void
+ */
+
+void free_list(listadd_t *head)
+{
+	listadd_t *list_free;
+
+	while (head != NULL)
+	{
+		list_free = head;
+		head = head->next;
+		free(list_free);
+	}
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -17,6 +17,19 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/**
+ * struct add_list_s - singly linked list
+ *
+ * @ptr: Address of a node
+ * @next: Pointer to next node
+ */
+
+typedef struct add_list_s
+{
+	void *ptr;
+	struct add_list_s *next;
+} listadd_t;
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -32,5 +45,7 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+void free_list(listadd_t *head);
+listadd_t *add_node(listadd_t **head, const listint_t *ptr);
 
 #endif
