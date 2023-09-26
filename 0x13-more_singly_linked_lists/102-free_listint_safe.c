74b05bcdeaa727a05b9dcbf82bc3a20ab85d5e13
Bruka25 <bkteshome862@gmail.com>
2023-09-26 09:49:56 -0700

    free list safe version changed code

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -13,43 +13,30 @@
 
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *remove_node;
+	listint_t *temp;
 	listint_t *current_node;
-	listadd_t *add_head;
-	listadd_t *check_loop;
 	size_t count;
 
-	count = 0;
+	if (h == NULL || *h == NULL)
+		return (0);
+
 	current_node = *h;
-	add_head = NULL;
-	if (h != NULL)
+	*h = NULL;
+
+	while (current_node != NULL)
 	{
-		while (current_node != NULL)
+		count++;
+		temp = current_node;
+		current_node = current_node->next;
+
+		if (temp <= current_node)
 		{
-			check_loop = add_head;
-			while (check_loop != NULL)
-			{
-				if (current_node == check_loop->ptr)
-				{
-					free(current_node);
-					free_list(add_head);
-					 *h = NULL;
-					return (count);
-				}
-				check_loop = check_loop->next;
-			}
-			remove_node = current_node;
-			if (add_node(&add_head, current_node) == NULL)
-			{
-				free_list(add_head);
-				exit(98);
-			}
-			current_node = current_node->next;
-			free(remove_node);
-			count++;
+			*h = NULL;
+			break;
 		}
-		free_list(add_head);
-		*h = NULL;
+
+		free(temp);
 	}
+
 	return (count);
 }
