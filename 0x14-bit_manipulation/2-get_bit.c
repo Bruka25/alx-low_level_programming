413e3dc88eb743e671481f28d570d39f365994b7
Bruka25 <bkteshome862@gmail.com>
2023-09-29 09:27:52 -0700

    get specific bit at specific index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,7 +11,7 @@
  *Return: Value of bit or -1 if error occured
  */
 
-int get_bit(unsigned long int n, unsigned int index);
+int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int bit_track;
 
