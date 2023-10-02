f13141442781d08f98f2f02bb88d5d6319304f78
Bruka25 <bkteshome862@gmail.com>
2023-10-02 08:13:14 -0700

    read and print textfile to a standard output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *read_textfile - Reads textfile and prints it to POSIX stdout
+ *
+ *@filename: Pointer to file argument
+ *@letters: Number of letters it should read and print
+ *
+ *Return: Actual number of letters it could read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file;
+	ssize_t read_c, write_c;
+	char *temp;
+
+	if (filename == NULL)
+		return (0);
+
+	file = open(filename, O_RDWR);
+	if (file == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (temp == NULL)
+	{
+		free(temp);
+		return (0);
+	}
+	read_c = read(file, temp, letters);
+	if (read_c == -1)
+		return (0);
+
+	write_c = write(STDOUT_FILENO, temp, read_c);
+	if (write_c == -1 || read_c != write_c)
+		return (0);
+	free(temp);
+
+	close(file);
+	return (write_c);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,7 @@
+The following functions are C programs for ALX 0x15-file_io project, inside it contains the following C functions:
+
+* A function that reads a text file and prints it to the POSIX standard output
+* A function that creates a file
+* A function that appends text at the end of a file
+* A program that copies the content of a file to another file
+* A program that displays the information contained in the ELF header at the start of an ELF file
