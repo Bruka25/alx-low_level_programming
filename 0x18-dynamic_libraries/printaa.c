fc664c16f8602d19217552b7ca989363005488e6
Bruka25 <bkteshome862@gmail.com>
2023-11-13 08:28:03 -0800

    win the jackpot

--- 0x18-dynamic_libraries/printaa.c
+++ 0x18-dynamic_libraries/printaa.c
@@ -1,12 +1,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int _print(const char format, ...);
+int _print(const char *format, ...);
 
-int _print(const char format, ...)
+int _print(const char *format, ...)
 {
-	write(1, "9 8 10 24 75 9\n", 16);
-	write(1, "congratulations you win the jackpot!\n", 38);
+    write(1, "9 8 10 24 75 9\n", 16);
+    write(1, "congratulations you win the jackpot!\n", 38);
 
-	exit(EXIT_SUCCESS);
+    exit(EXIT_SUCCESS);
 }
