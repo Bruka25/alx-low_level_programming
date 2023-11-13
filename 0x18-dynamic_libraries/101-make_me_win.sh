9f352c3fe98f91aaccae27a3287033bb5fb61f5e
Bruka25 <bkteshome862@gmail.com>
2023-11-13 11:25:41 -0800

    jackpot win update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O /tmp/winlib.so https://github.com/Bruka25/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libwin.so 
-export LD_PRELOAD=/tmp/winlib.so
+wget -P .. https://github.com/Bruka25/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libwin.so 
+export LD_PRELOAD="$PWD/../libwin.so
