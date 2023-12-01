edb3d1436b76db450073b0cacfd92ce7e42a2827
Bruka25 <bkteshome862@gmail.com>
2023-12-01 14:57:59 +0300

    print hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,42 @@
+#include "hash_tables.h"
+#include <stdio.h>
+
+/**
+ *hash_table_print - Functions that Prints a hash table
+ *
+ *@ht: The hash table
+ *
+ *Return: Void
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *temp;
+	unsigned long int h;
+	int switch;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+
+	for (h = 0, switch = 0; h < ht->size; h++)
+	{
+		temp = ht->array[i];
+		if (temp != NULL)
+		{
+			if (switch == 1)
+				printf(", ");
+			printf("'%s': '%s'", temp->key, temp->value);
+			while ((temp = temp->next) != NULL)
+			{
+				printf(", ");
+				printf("'%s': '%s'", temp->key, temp->value);
+			}
+
+			switch = 1;
+		}
+	}
+
+	printf("}\n");
+}
