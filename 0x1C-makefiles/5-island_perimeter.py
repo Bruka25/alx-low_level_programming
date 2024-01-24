9514d5948b4d373073eaad2ef6bf40303bc64524
Bruka25 <bkteshome862@gmail.com>
2024-01-24 22:56:13 +0300

    perimeter island founder fix

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-""" Module for finding the perimmeter of
+""" Module for finding the perimeter of
     an island
 """
 
@@ -13,10 +13,17 @@ def island_perimeter(grid):
 
     for i in range(len(grid)):
         for j in range(len(grid[0])):
-            if grid[i][j] == 1:
+
+            if i >= len(grid) or j >= len(grid[0]):
+                continue
+            elif i < 0 or j < 0 or grid[i][j] == 0:
+                continue
+
+            elif grid[i][j] == 1:
                 perim_count += 4
                 if i > 0 and grid[i - 1][j] == 1:
                     perim_count -= 2
                 if j > 0 and grid[i][j - 1] == 1:
-                    perim_countr -= 2
+                    perim_count -= 2
+
     return perim_count
