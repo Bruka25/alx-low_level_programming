6900b1d8071b2469929357ae1eab1c4f90d63dc5
Bruka25 <bkteshome862@gmail.com>
2024-03-12 18:09:26 +0300

    searching algorithm function to search a value usong binary search method

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,6 +10,8 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
+void array_print(int *array, size_t idx, size_t size);
+void print_array(int *array, size_t inc, size_t size);
 /**
  * struct listint_s - singly linked list
  *
